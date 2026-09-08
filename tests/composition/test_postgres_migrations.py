cef7a249a5d4c7e8bb66fedf0ceffe9604f38fd0
aaterekhin <Lehater@mail.ru>
2026-09-09 01:55:26 +0300

    test(migrations): include ACC display metadata migration

--- tests/composition/test_postgres_migrations.py
+++ tests/composition/test_postgres_migrations.py
@@ -48,6 +48,7 @@ def test_migration_registry_has_stable_unique_ids():
         "access-policy/0001",
         "access-policy/0002",
         "access-policy/0003",
+        "application-catalogue/0002",
         "authority-management/0001",
         "application-catalogue/0001",
         "resource-catalogue/0001",
