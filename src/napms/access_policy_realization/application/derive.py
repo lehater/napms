ebad8264a661d15435b0be7fb2ebd1637af95760
aaterekhin <Lehater@mail.ru>
2026-09-09 23:52:10 +0300

    i20: implement realization reconciliation core

---
+++ src/napms/access_policy_realization/application/derive.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from napms.access_policy_realization.domain.model import (
+    DomainInteractionIdentity,
+)
+from napms.access_policy_realization.domain.realization import (
+    DesiredEnforcementPolicy,
+    DesiredPolicyContribution,
+    derive_desired_enforcement_policy,
+)
+
+
+class DeriveDesiredEnforcementPolicy:
+    def execute(
+        self,
+        *,
+        governance_scope: str,
+        as_of: datetime,
+        desired_interactions: tuple[
+            DomainInteractionIdentity,
+            ...,
+        ],
+        contributions: tuple[
+            DesiredPolicyContribution,
+            ...,
+        ],
+    ) -> DesiredEnforcementPolicy:
+        return derive_desired_enforcement_policy(
+            governance_scope=governance_scope,
+            as_of=as_of,
+            desired_interactions=desired_interactions,
+            contributions=contributions,
+        )
