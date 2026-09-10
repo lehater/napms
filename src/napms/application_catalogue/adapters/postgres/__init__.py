14f557b9fe54bd3d003fffb34be372d895f126f6
aaterekhin <Lehater@mail.ru>
2026-09-10 15:30:10 +0300

    I27 export ACC curation repository

--- src/napms/application_catalogue/adapters/postgres/__init__.py
+++ src/napms/application_catalogue/adapters/postgres/__init__.py
@@ -1,5 +1,11 @@
+from napms.application_catalogue.adapters.postgres.curation_repository import (
+    PostgresApplicationCatalogueCurationRepository,
+)
 from napms.application_catalogue.adapters.postgres.repository import (
     PostgresApplicationCatalogueRepository,
 )
 
-__all__ = ["PostgresApplicationCatalogueRepository"]
+__all__ = [
+    "PostgresApplicationCatalogueCurationRepository",
+    "PostgresApplicationCatalogueRepository",
+]
