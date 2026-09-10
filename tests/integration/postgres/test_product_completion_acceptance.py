36968af20f7f7dda630038567a1d6a67c7a1c8a4
aaterekhin <Lehater@mail.ru>
2026-09-10 16:50:54 +0300

    test(i27): clean ACC hierarchy in product acceptance

--- tests/integration/postgres/test_product_completion_acceptance.py
+++ tests/integration/postgres/test_product_completion_acceptance.py
@@ -122,7 +122,9 @@ def clean_product_completion_state(postgres_dsn, config):
             TRUNCATE TABLE
                 napms_application_catalogue.deployment_resource_bindings,
                 napms_application_catalogue.dcs_revisions,
-                napms_application_catalogue.component_deployments
+                napms_application_catalogue.component_deployments,
+                napms_application_catalogue.components,
+                napms_application_catalogue.applications
             CASCADE
             """
         )
