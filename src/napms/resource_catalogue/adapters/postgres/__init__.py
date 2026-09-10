2d17dd804d852e1c4ea8f7958ffc79dec749c3e3
aaterekhin <Lehater@mail.ru>
2026-09-10 15:35:18 +0300

    I27 export Resource Catalogue curation repository

--- src/napms/resource_catalogue/adapters/postgres/__init__.py
+++ src/napms/resource_catalogue/adapters/postgres/__init__.py
@@ -1,5 +1,11 @@
+from napms.resource_catalogue.adapters.postgres.curation_repository import (
+    PostgresResourceCatalogueCurationRepository,
+)
 from napms.resource_catalogue.adapters.postgres.repository import (
     PostgresResourceCatalogueRepository,
 )
 
-__all__ = ["PostgresResourceCatalogueRepository"]
+__all__ = [
+    "PostgresResourceCatalogueCurationRepository",
+    "PostgresResourceCatalogueRepository",
+]
