ed7c1981a6faf447440053eebd95586ed85bacdc
aaterekhin <Lehater@mail.ru>
2026-09-10 15:26:16 +0300

    I27 expect ACC curation migration

--- tests/composition/test_postgres_migrations.py
+++ tests/composition/test_postgres_migrations.py
@@ -52,6 +52,7 @@ def test_migration_registry_has_stable_unique_ids():
         "application-catalogue/0001",
         "application-catalogue/0002",
         "application-catalogue/0003",
+        "application-catalogue/0004",
         "resource-catalogue/0001",
         "resource-catalogue/0002",
         "connectivity-requirements/0001",
