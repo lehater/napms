a5cd90a1df6ee36b35c4930886da3227511eebc3
aaterekhin <Lehater@mail.ru>
2026-09-11 00:06:47 +0300

    I31 M2: export target PostgreSQL repository

--- src/napms/application_catalogue/adapters/postgres/__init__.py
+++ src/napms/application_catalogue/adapters/postgres/__init__.py
@@ -4,8 +4,12 @@ from napms.application_catalogue.adapters.postgres.curation_repository import (
 from napms.application_catalogue.adapters.postgres.repository import (
     PostgresApplicationCatalogueRepository,
 )
+from napms.application_catalogue.adapters.postgres.target_repository import (
+    PostgresTargetApplicationCatalogueRepository,
+)
 
 __all__ = [
     "PostgresApplicationCatalogueCurationRepository",
     "PostgresApplicationCatalogueRepository",
+    "PostgresTargetApplicationCatalogueRepository",
 ]
