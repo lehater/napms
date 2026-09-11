82a1d284056e9b89fdf627d516e10bef67cb5056
aaterekhin <Lehater@mail.ru>
2026-09-11 03:16:28 +0300

    test: fix application screenshot baselines

--- e2e/screenshot_regression.py
+++ e2e/screenshot_regression.py
@@ -5,9 +5,9 @@ from playwright.sync_api import Page
 
 
 APPLICATION_SCREEN_BASELINES: dict[str, str] = {
-    "definitions-list": "",
-    "definition-interactions": "",
-    "deployment-connectivity": "",
+    "definitions-list": "0001800e902e100ea00180061921c682c2422db42db4cc4b0493810301030000",
+    "definition-interactions": "90008c008002c1028480b080820e4601802c932c33099107130b130781030001",
+    "deployment-connectivity": "90008530c2ca928199914b11806c802c489b9241946a942a1c261c6281030001",
 }
 MAX_HAMMING_DISTANCE = 6
 
