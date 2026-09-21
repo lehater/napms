e95446b69f837c9ee840186f40b4a3dc4865ace9
aaterekhin <Lehater@mail.ru>
2026-09-21 22:25:18 +0300

    refactor: route application and connectivity workspaces

--- web/src/app/main.tsx
+++ web/src/app/main.tsx
@@ -2,6 +2,13 @@ import { StrictMode, useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { AppShell } from "../components/AppShell";
 import {
+  ApplicationCatalogue,
+  ApplicationDetail,
+  InteractionAuthoring,
+} from "../features/applications/ApplicationScreens";
+import { BusinessConnectivity } from "../features/business-connectivity/BusinessConnectivity";
+import { DeploymentScreens } from "../features/deployments/DeploymentScreens";
+import {
   ResourceCatalogue,
   ResourceDetail,
 } from "../features/resources/ResourceScreens";
@@ -26,6 +33,30 @@ function App() {
     screen = <ResourceCatalogue create />;
   } else if (route.id === "resource-detail") {
     screen = <ResourceDetail resourceRef={route.resourceRef} />;
+  } else if (route.id === "applications") {
+    screen = <ApplicationCatalogue />;
+  } else if (route.id === "application-new") {
+    screen = <ApplicationCatalogue create />;
+  } else if (route.id === "application-detail") {
+    screen = <ApplicationDetail applicationRef={route.applicationRef} />;
+  } else if (route.id === "component-new") {
+    screen = (
+      <ApplicationDetail applicationRef={route.applicationRef} createComponent />
+    );
+  } else if (route.id === "interaction-new") {
+    screen = <InteractionAuthoring />;
+  } else if (route.id === "revision-new") {
+    screen = <InteractionAuthoring interactionRef={route.interactionRef} />;
+  } else if (route.id === "deployments") {
+    screen = <DeploymentScreens />;
+  } else if (route.id === "deployment-new") {
+    screen = <DeploymentScreens create />;
+  } else if (route.id === "processes") {
+    screen = <BusinessConnectivity />;
+  } else if (route.id === "process-new") {
+    screen = <BusinessConnectivity create />;
+  } else if (route.id === "process-detail") {
+    screen = <BusinessConnectivity processRef={route.processRef} />;
   } else {
     screen = (
       <section className="panel">
