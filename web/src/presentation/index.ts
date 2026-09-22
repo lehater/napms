aa20ea30adab0e3c334a8663865d50ca478ca21e
aaterekhin <Lehater@mail.ru>
2026-09-22 14:13:28 +0300

    Organize presentation exports

--- web/src/presentation/index.ts
+++ web/src/presentation/index.ts
@@ -9,5 +9,5 @@ export type {
 } from "./contracts";
 export { MuiAppShell as AppShell } from "./providers/mui/MuiAppShell";
 export { MuiCataloguePattern as CataloguePattern } from "./providers/mui/MuiCataloguePattern";
-export { MuiPresentationRoot as PresentationRoot } from "./providers/mui/MuiPresentationRoot";
 export { MuiEditorPattern as EditorPattern } from "./providers/mui/MuiEditorPattern";
+export { MuiPresentationRoot as PresentationRoot } from "./providers/mui/MuiPresentationRoot";
