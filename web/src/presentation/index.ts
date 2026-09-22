2ba3ef14a3b7196aa3af35bd1b3c7858819de046
aaterekhin <Lehater@mail.ru>
2026-09-22 14:04:23 +0300

    Expose editor pattern through presentation facade

--- web/src/presentation/index.ts
+++ web/src/presentation/index.ts
@@ -3,7 +3,11 @@ export type {
   CatalogueColumn,
   CataloguePatternProps,
   CatalogueState,
+  EditorField,
+  EditorPatternProps,
+  EditorState,
 } from "./contracts";
 export { MuiAppShell as AppShell } from "./providers/mui/MuiAppShell";
 export { MuiCataloguePattern as CataloguePattern } from "./providers/mui/MuiCataloguePattern";
 export { MuiPresentationRoot as PresentationRoot } from "./providers/mui/MuiPresentationRoot";
+export { MuiEditorPattern as EditorPattern } from "./providers/mui/MuiEditorPattern";
